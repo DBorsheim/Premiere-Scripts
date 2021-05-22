23bd00137eac7ad95dcfa4909f14d220a11dccb6
DBorsheim <33246513+DBorsheim@users.noreply.github.com>
2021-05-22 14:26:13 -0500

    Update Set Interpolation Type.jsx

--- Set Interpolation Type.jsx
+++ Set Interpolation Type.jsx
@@ -1,1 +1,2 @@
 https://ppro-scripting.docsforadobe.dev/sequence/componentparam.html?highlight=keyframe#componentparam-setinterpolationtypeatkey
+https://www.youtube.com/watch?v=cmwzXL6pRfw&list=PL0qACgPuF8dXiuFb4rR8JB95BZ9Gr-G3d&index=13
